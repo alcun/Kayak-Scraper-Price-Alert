0d179209beecfae421cbd3477ca5b8c1250afe19
alcun <95560342+alcun@users.noreply.github.com>
2021-12-13 19:52:02 +0000

    prints title with .text.strip and spam commented

--- price-scraper.py
+++ price-scraper.py
@@ -2,11 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-import requests
-from bs4 import BeautifulSoup
-
-
-#imports a lot of stuff atm - good luck making sense of it!
+#imports a lot of stuff atm, run at your own risk
 
 URL = "https://www.amazon.co.uk/Intex-Explorer-Two-Person-Kayak-Oars/dp/B00AIQ8LGG/ref=sr_1_2?keywords=kayak&qid=1639423847&sr=8-2"
 
@@ -18,21 +14,9 @@ page = requests.get(URL, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-print(soup.prettify())
+#print(soup.prettify())
 
 
-URL = "https://www.amazon.co.uk/Intex-Explorer-Two-Person-Kayak-Oars/dp/B00AIQ8LGG/ref=sr_1_2?keywords=kayak&qid=1639423847&sr=8-2"
+title = soup.find (id="productTitle")
 
-headers = {
-  "User-Agent": Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36}
-
-
-page = requests.get(URL, headers=headers)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-print(soup.prettify())
-
-title = soup.find (id="productTitle")   
-
-print(title)
+print(title.text.strip())
